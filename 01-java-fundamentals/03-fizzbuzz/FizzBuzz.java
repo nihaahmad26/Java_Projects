ef36d8ec3e88855cbb055cd1dcc4fe000f7c9bda
Niha Ahmad <nihaahmad26@gmail.com>
2021-09-01 17:41:35 -0500

    FizzBuzz Assignment

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz{
+    public String fizzBuzz(int number){
+        if((number % 3 == 0) && (number % 5 == 0)){
+            return("FizzBuzz");
+        }
+        else if(number % 5 == 0){
+            return("Buzz");
+        }
+        else if (number % 3 == 0){
+            return("Fizz");
+        }
+        return Integer.toString(number);
+    }
+}
---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz tester = new FizzBuzz();
+        System.out.println(tester.fizzBuzz(9));
+        System.out.println(tester.fizzBuzz(10));
+        System.out.println(tester.fizzBuzz(15));
+        System.out.println(tester.fizzBuzz(16));
+        System.out.println(tester.fizzBuzz(2));
+    }
+}
+
